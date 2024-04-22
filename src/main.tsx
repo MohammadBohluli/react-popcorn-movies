d2b0169b15df60488584bd01f5ffaadc4acfa7b2
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-04-22 15:26:56 +0330

    create Home component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+
+const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+import CardList from '../components/CardList';
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <CardList />
+    </div>
+  );
+};
+
+export default Home;
