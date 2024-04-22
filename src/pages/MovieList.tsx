76b5dab3184e4a7032ab7c4a97efa4207a20dab5
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-04-23 01:19:53 +0330

    refactor: pagination with react-router-dom

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -2,15 +2,26 @@ import { Box, Grid, Pagination, Stack } from '@mui/material';
 import { useGetMovieListQuery } from '../feather/movies/movie-api-slice';
 import CradMovie from '../components/CardMovie';
 import CardSkleton from '../components/CardSkleton';
-import { useState } from 'react';
+
 import ShowError from '../components/ShowError';
+import { useSearchParams } from 'react-router-dom';
 
 const MovieList = () => {
-  const [page, setPage] = useState<number>(1);
-  const { data, isLoading, isError } = useGetMovieListQuery(page);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageNumber = searchParams.get('page') ?? 1;
+  const { data, isLoading, isError } = useGetMovieListQuery(Number(pageNumber));
 
   const movies = data?.results ? data.results : [];
   const skletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const handlePageChange = (
+    _event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    searchParams.set('page', page.toString());
+    setSearchParams(searchParams);
+  };
 
   if (isError) return <ShowError />;
 
@@ -35,7 +46,7 @@ const MovieList = () => {
       <Stack my={7}>
         <Pagination
           count={data?.total_pages}
-          onChange={(_, page) => setPage(page)}
+          onChange={handlePageChange}
           siblingCount={3}
           size="large"
           color="primary"
